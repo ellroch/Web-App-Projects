fe332436471115be583536ffdde7cd8feeec7490
Chris <chrisglanzer@privateinfosec.com>
2024-04-15 17:33:20 -0500

    7

---
+++ chapter 7/XEx06Reservation/XEx06Reservation/XEx06Reservation/XEx06Reservation/Request.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace XEx06Reservation
+{
+    public partial class Request : System.Web.UI.Page
+    {
+        private string currentDate = DateTime.Today.ToShortDateString();
+        private string currentYear = DateTime.Today.Year.ToString();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            txtDeparturDate.Text = currentDate;
+            txtArrivalDate.Text = currentDate;
+            rblBedType.SelectedIndex = 0;
+            lblYear.Text = currentYear;
+            ddlNumOfPeople.SelectedIndex = 0;
+            ddlPrefferedMethod.SelectedIndex = 0;
+            txtEmail.Text = "";
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+            txtPhone.Text = "";
+            txtSpecialRequests.Text = "";
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = "Submission Recieved, Thank you for your time.";
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            txtDeparturDate.Text = currentDate;
+            txtArrivalDate.Text = currentDate;
+            rblBedType.SelectedIndex = 0;
+            lblYear.Text = currentYear;
+            ddlNumOfPeople.SelectedIndex = 0;
+            ddlPrefferedMethod.SelectedIndex = 0;
+            txtEmail.Text = "";
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+            txtPhone.Text = "";
+            txtSpecialRequests.Text = "";
+        }
+    }
+}
